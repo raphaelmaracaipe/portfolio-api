caf73abcc1ce3ce8e7a735fb69c65502a5e81945
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2023-11-26 16:56:56 -0300

    Criação de endpoint para incluir o profile.

---
+++ src/user-profile/services/profie.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { Profile } from "../models/profile.interface";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "../../core/models/user.model";
+import { MongoRepository } from "typeorm";
+import { ExceptionBadRequest } from "../../core/exeptions/exceptionBadRequest";
+import { Codes } from "../../core/codes/codes";
+
+@Injectable()
+export class ProfileService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: MongoRepository<User>,
+    private readonly codes: Codes
+  ) { }
+
+  async insert(deviceId: string, profile: Profile) {
+    try {
+      const { photo, name } = profile
+      await this.userRepository.updateOne({
+        deviceId
+      }, {
+        $set: {
+          photo,
+          name
+        }
+      })
+    } catch (e) {
+      throw new ExceptionBadRequest(this.codes.USER_FAIL_TO_INSERT_PROFILE)
+    }
+  }
+
+}
