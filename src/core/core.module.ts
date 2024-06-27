2ffc880b3c72b69b020e5486d84c7501d570f144
Raphael Maracaipe <raphaelmaracaipe@gmail.com>
2024-06-27 19:59:19 -0300

    Ajuste na referência do provider.

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -11,6 +11,10 @@ import { Codes } from './codes/codes';
 import { LbCryptoService } from '@app/lb-crypto';
 import { RegexService } from './regex/regex.service';
 import { ApiKey } from './models/apiKey.model';
+import { CleanKeySchedule } from './schedules/clean_keys/clean_keys.schedule';
+import { CleanKeyService } from './schedules/clean_keys/clean_keys.service';
+import { CodesExpirationsSchedule } from './schedules/codes_expirations/codes_expirations.schedule';
+import { CodesExpirationsService } from './schedules/codes_expirations/codes_expirations.service';
 
 @Module({
   imports: [
@@ -29,6 +33,10 @@ import { ApiKey } from './models/apiKey.model';
     Codes,
     LbCryptoService,
     RegexService,
+    CleanKeySchedule,
+    CleanKeyService,
+    CodesExpirationsSchedule,
+    CodesExpirationsService
   ],
 })
 export class CoreModule {}
